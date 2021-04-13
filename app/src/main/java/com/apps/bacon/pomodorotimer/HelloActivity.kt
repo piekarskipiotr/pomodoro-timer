7858997d77242437f6f8ab86af757bc22d51fa3a
piekarskipiotr <ppiekarski8@gmail.com>
2021-04-13 18:21:37 +0200

    Create first run activity

--- app/src/main/java/com/apps/bacon/pomodorotimer/HelloActivity.kt
+++ app/src/main/java/com/apps/bacon/pomodorotimer/HelloActivity.kt
@@ -2,16 +2,15 @@ package com.apps.bacon.pomodorotimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.apps.bacon.pomodorotimer.databinding.ActivityHomeBinding
+import com.apps.bacon.pomodorotimer.databinding.ActivityHelloBinding
 
-class HomeActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityHomeBinding
+class HelloActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHelloBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityHelloBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
     }
 }
