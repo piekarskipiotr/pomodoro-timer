e919f4a1fabd3137adb3b598ce7d72e2f6759a3c
piekarskipiotr <ppiekarski8@gmail.com>
2021-04-15 20:21:19 +0200

    Create time selector

--- app/src/main/java/com/apps/bacon/pomodorotimer/HomeActivity.kt
+++ app/src/main/java/com/apps/bacon/pomodorotimer/HomeActivity.kt
@@ -5,10 +5,18 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
 import com.apps.bacon.pomodorotimer.databinding.ActivityHomeBinding
+import com.apps.bacon.pomodorotimer.databinding.DialogSetTimerBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
     private lateinit var timer: CountDownTimer
+
+    enum class Time(val milliseconds: Long) {
+        TwentyFive(1500000),
+        Fifty(3000000)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +25,22 @@ class HomeActivity : AppCompatActivity() {
         setContentView(view)
 
         binding.startTimerButton.setOnClickListener {
-            startTimer()
+            val dialogBinding = DialogSetTimerBinding.inflate(layoutInflater)
+            val dialogView = dialogBinding.root
+            val bottomSheetDialog = BottomSheetDialog(this, R.style.BottomSheetDialogTheme)
+            bottomSheetDialog.setContentView(dialogView)
+            bottomSheetDialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
+            bottomSheetDialog.show()
+
+            dialogBinding.twentyFiveMinutesChip.setOnClickListener {
+                startTimer(Time.TwentyFive.milliseconds)
+                bottomSheetDialog.dismiss()
+            }
+
+            dialogBinding.fiftyMinutesChip.setOnClickListener {
+                startTimer(Time.Fifty.milliseconds)
+                bottomSheetDialog.dismiss()
+            }
         }
 
         binding.stopTimerButton.setOnClickListener {
@@ -39,12 +62,12 @@ class HomeActivity : AppCompatActivity() {
         resetUI()
     }
 
-    private fun startTimer() {
+    private fun startTimer(milliseconds: Long) {
         updateStartButton(false, DISABLE_BUTTON_ALPHA)
         updateStopButton(true, ENABLE_BUTTON_ALPHA)
 
-        binding.circularProgressBar.progressMax = 1500f
-        timer = object : CountDownTimer(1500000, 1000) {
+        binding.circularProgressBar.progressMax = (milliseconds / 1000).toFloat()
+        timer = object : CountDownTimer(milliseconds, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 updateUI(millisUntilFinished)
             }
@@ -55,24 +78,24 @@ class HomeActivity : AppCompatActivity() {
         }.start()
     }
 
-    private fun updateUI(ms: Long) {
-        val minutes = (ms / 1000) / 60
-        val seconds = (ms / 1000) % 60
-        binding.timerText.text = String.format("%02d:%02d",  minutes, seconds)
+    private fun updateUI(milliseconds: Long) {
+        val minutes = (milliseconds / 1000) / 60
+        val seconds = (milliseconds / 1000) % 60
+        binding.timerText.text = String.format("%02d:%02d", minutes, seconds)
 
         binding.circularProgressBar.apply {
             setProgressWithAnimation(progress + 1, 500)
         }
     }
 
-    private fun updateStartButton(isClickable: Boolean, alpha: Float){
+    private fun updateStartButton(isClickable: Boolean, alpha: Float) {
         binding.startTimerButton.apply {
             this.isClickable = isClickable
             this.alpha = alpha
         }
     }
 
-    private fun updateStopButton(isClickable: Boolean, alpha: Float){
+    private fun updateStopButton(isClickable: Boolean, alpha: Float) {
         binding.stopTimerButton.apply {
             this.isClickable = isClickable
             this.alpha = alpha
