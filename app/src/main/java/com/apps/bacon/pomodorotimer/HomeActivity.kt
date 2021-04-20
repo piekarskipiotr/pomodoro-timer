06ebef11a70823eb8f370f319dd0d772a5c193a3
piekarskipiotr <ppiekarski8@gmail.com>
2021-04-20 19:51:47 +0200

    Handle completed session dialog

--- app/src/main/java/com/apps/bacon/pomodorotimer/HomeActivity.kt
+++ app/src/main/java/com/apps/bacon/pomodorotimer/HomeActivity.kt
@@ -11,10 +11,12 @@ import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.apps.bacon.pomodorotimer.databinding.ActivityHomeBinding
+import com.apps.bacon.pomodorotimer.databinding.DialogCompletedSessionBinding
 import com.apps.bacon.pomodorotimer.databinding.DialogSetTimerBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
@@ -23,15 +25,19 @@ class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
     private lateinit var timer: CountDownTimer
     private lateinit var sharedPreference: SharedPreferences
+    private var msUntilFinished: Long = 0
 
     enum class Time(val milliseconds: Long) {
         TwentyFive(1500000),
-        Fifty(3000000)
+        Fifty(3000000),
+        ShortBreak(300000),
+        LongBreak(1800000)
     }
 
     enum class TimerState {
         RUNNING,
         IN_THE_BACKGROUND,
+        FINISHED_IN_THE_BACKGROUND,
         STOPPED
     }
 
@@ -53,18 +59,20 @@ class HomeActivity : AppCompatActivity() {
 
             dialogBinding.twentyFiveMinutesChip.setOnClickListener {
                 startTimer(Time.TwentyFive.milliseconds)
+                setSessionInfo(Time.TwentyFive.milliseconds)
                 bottomSheetDialog.dismiss()
             }
 
             dialogBinding.fiftyMinutesChip.setOnClickListener {
                 startTimer(Time.Fifty.milliseconds)
+                setSessionInfo(Time.Fifty.milliseconds)
                 bottomSheetDialog.dismiss()
             }
         }
 
         binding.stopTimerButton.setOnClickListener {
             timer.cancel()
-            onTimerFinished()
+            resetUI()
         }
 
         binding.userProfileButton.setOnClickListener {
@@ -73,6 +81,15 @@ class HomeActivity : AppCompatActivity() {
         }
 
         updateStopButton(false, DISABLE_BUTTON_ALPHA)
+    }
+
+    private fun setSessionInfo(sessionTime: Long) {
+        with(sharedPreference.edit()) {
+            putLong(SESSION_TIME_KEY, sessionTime)
+            putInt(SESSION_COUNT_KEY, 1)
+            putBoolean(IS_BREAK_KEY, false)
+            apply()
+        }
     }
 
     private fun startTimer(milliseconds: Long) {
@@ -86,6 +103,7 @@ class HomeActivity : AppCompatActivity() {
         binding.circularProgressBar.progressMax = (milliseconds / 1000).toFloat()
         timer = object : CountDownTimer(milliseconds, 1000) {
             override fun onTick(millisUntilFinished: Long) {
+                msUntilFinished = millisUntilFinished
                 updateUI(millisUntilFinished)
             }
 
@@ -96,19 +114,33 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun onTimerFinished() {
-        updateStopButton(false, DISABLE_BUTTON_ALPHA)
-        updateStartButton(true, ENABLE_BUTTON_ALPHA)
         resetUI()
+        val timerForBreak = sharedPreference.getBoolean(IS_BREAK_KEY, false)
 
-        val timerState = sharedPreference.getString(TIMER_STATE_KEY, TimerState.STOPPED.name)
-        if (timerState == TimerState.IN_THE_BACKGROUND.name)
-            sendNotification(
-                getString(R.string.notify_title),
-                getString(R.string.notify_description)
-            )
+        if (timerForBreak)
+            showBreakDialog()
+        else
+            showCompletedSessionDialog()
+
+        val currentTimerState = sharedPreference.getString(TIMER_STATE_KEY, TimerState.STOPPED.name)
+        var timerState = TimerState.STOPPED.name
+        if (currentTimerState == TimerState.IN_THE_BACKGROUND.name) {
+            timerState = TimerState.FINISHED_IN_THE_BACKGROUND.name
+
+            if (timerForBreak)
+                sendNotification(
+                    getString(R.string.break_notify_title),
+                    getString(R.string.break_notify_description)
+                )
+            else
+                sendNotification(
+                    getString(R.string.session_notify_title),
+                    getString(R.string.session_notify_description)
+                )
+        }
 
         with(sharedPreference.edit()) {
-            putString(TIMER_STATE_KEY, TimerState.STOPPED.name)
+            putString(TIMER_STATE_KEY, timerState)
             apply()
         }
     }
@@ -124,6 +156,8 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun resetUI() {
+        updateStopButton(false, DISABLE_BUTTON_ALPHA)
+        updateStartButton(true, ENABLE_BUTTON_ALPHA)
         binding.timerText.text = getString(R.string.time_zero)
         binding.circularProgressBar.apply {
             setProgressWithAnimation(0f, 500)
@@ -144,6 +178,55 @@ class HomeActivity : AppCompatActivity() {
         }
     }
 
+    private fun showCompletedSessionDialog() {
+        val alertDialog: AlertDialog
+        val builder = AlertDialog.Builder(this, R.style.DialogStyle)
+        val dialogBinding = DialogCompletedSessionBinding.inflate(LayoutInflater.from(this))
+        val view = dialogBinding.root
+        builder.setView(view)
+        alertDialog = builder.create()
+        alertDialog.setCanceledOnTouchOutside(false)
+
+        with(sharedPreference.edit()) {
+            putString(TIMER_STATE_KEY, TimerState.STOPPED.name)
+            apply()
+        }
+
+        val sessionCount = sharedPreference.getInt(SESSION_COUNT_KEY, 1)
+        var breakTime = Time.ShortBreak.milliseconds
+
+        if (sessionCount % 4 == 0) {
+            breakTime = Time.LongBreak.milliseconds
+            dialogBinding.breakButtonText.text = getString(R.string.take_long_break)
+        }
+
+        dialogBinding.skipBreakButton.setOnClickListener {
+            val sessionTime = sharedPreference.getLong(SESSION_TIME_KEY, 0L)
+            with(sharedPreference.edit()) {
+                putInt(SESSION_COUNT_KEY, sessionCount.inc())
+                apply()
+            }
+            startTimer(sessionTime)
+            alertDialog.dismiss()
+        }
+
+        dialogBinding.takeBreakButton.setOnClickListener {
+            with(sharedPreference.edit()) {
+                putInt(SESSION_COUNT_KEY, sessionCount.inc())
+                putBoolean(IS_BREAK_KEY, true)
+                apply()
+            }
+            startTimer(breakTime)
+            alertDialog.dismiss()
+        }
+
+        dialogBinding.finishButton.setOnClickListener {
+            alertDialog.dismiss()
+        }
+
+        alertDialog.show()
+    }
+
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
@@ -161,7 +244,6 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun sendNotification(title: String, description: String) {
-        val intent = Intent(this, HomeActivity::class.java)
         val pendingIntent = TaskStackBuilder.create(this).run {
             addNextIntentWithParentStack(intent)
             getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
@@ -186,10 +268,10 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onPause() {
         super.onPause()
-        val timerState = sharedPreference.getString("TIMER_STATE", TimerState.STOPPED.name)
+        val timerState = sharedPreference.getString(TIMER_STATE_KEY, TimerState.STOPPED.name)
         if (timerState == TimerState.RUNNING.name) {
             with(sharedPreference.edit()) {
-                putString("TIMER_STATE", TimerState.IN_THE_BACKGROUND.name)
+                putString(TIMER_STATE_KEY, TimerState.IN_THE_BACKGROUND.name)
                 apply()
             }
         }
@@ -197,12 +279,19 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        val timerState = sharedPreference.getString("TIMER_STATE", TimerState.STOPPED.name)
+        val timerState = sharedPreference.getString(TIMER_STATE_KEY, TimerState.STOPPED.name)
         if (timerState == TimerState.IN_THE_BACKGROUND.name) {
             with(sharedPreference.edit()) {
-                putString("TIMER_STATE", TimerState.RUNNING.name)
+                putString(TIMER_STATE_KEY, TimerState.RUNNING.name)
                 apply()
             }
+        } else if (timerState == TimerState.FINISHED_IN_THE_BACKGROUND.name) {
+            val timerForBreak = sharedPreference.getBoolean(IS_BREAK_KEY, false)
+
+            if (timerForBreak)
+                showBreakDialog()
+            else
+                showCompletedSessionDialog()
         }
     }
 
@@ -211,6 +300,12 @@ class HomeActivity : AppCompatActivity() {
         private const val ENABLE_BUTTON_ALPHA = 1f
         private const val CHANNEL_ID = "CHANNEL_P1"
         private const val CHANNEL_NAME = "POMODORO_TIMER_CHANNEL"
+
+        //sharedPreference keys
         private const val TIMER_STATE_KEY = "TIMER_STATE"
+        private const val SESSION_TIME_KEY = "SESSION_TIME"
+        private const val SESSION_COUNT_KEY = "SESSION_COUNT"
+        private const val IS_BREAK_KEY = "IS_BREAK"
+
     }
 }
