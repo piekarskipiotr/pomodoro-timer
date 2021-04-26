95cfbddb3450738afe4ef746f40c95e3a631bba6
Piotr Piekarski <ppiekarski8@gmail.com>
2021-04-26 15:25:00 +0200

    Show user data in profile

--- app/src/main/java/com/apps/bacon/pomodorotimer/UserProfileActivity.kt
+++ app/src/main/java/com/apps/bacon/pomodorotimer/UserProfileActivity.kt
@@ -1,32 +1,64 @@
 package com.apps.bacon.pomodorotimer
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.apps.bacon.pomodorotimer.databinding.ActivityUserProfileBinding
+import com.apps.bacon.pomodorotimer.viewmodel.ProfileViewModel
 import com.github.mikephil.charting.charts.CombinedChart
 import com.github.mikephil.charting.charts.CombinedChart.DrawOrder
 import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class UserProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityUserProfileBinding
-    private lateinit var sharedPreference: SharedPreferences
+    private val profileViewModel: ProfileViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUserProfileBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        sharedPreference = this.getSharedPreferences("USER_INFO", Context.MODE_PRIVATE)
-        val userName = sharedPreference.getString("USER_NAME", getString(R.string.user)) as String
+        val userName = profileViewModel.getUserName()
         setStatsHelloText(userName)
-        initChart(binding.pomodoroChart)
+
+        profileViewModel.getCompletedSessions().observe(this, {
+            setCompletedSessions(it)
+        })
+
+        profileViewModel.getRunningSessions().observe(this, {
+            setRunningSessions(it)
+        })
+
+        profileViewModel.getCurrentWeekStats().observe(this, {
+            val runningData = listOf(
+                BarEntry(1f, it.mondayRunning.toFloat()),
+                BarEntry(2f, it.tuesdayRunning.toFloat()),
+                BarEntry(3f, it.wednesdayRunning.toFloat()),
+                BarEntry(4f, it.thursdayRunning.toFloat()),
+                BarEntry(5f, it.fridayRunning.toFloat()),
+                BarEntry(6f, it.saturdayRunning.toFloat()),
+                BarEntry(7f, it.sundayRunning.toFloat())
+            )
+
+            val completedData = listOf(
+                BarEntry(1f, it.mondayCompleted.toFloat()),
+                BarEntry(2f, it.tuesdayCompleted.toFloat()),
+                BarEntry(3f, it.wednesdayCompleted.toFloat()),
+                BarEntry(4f, it.thursdayCompleted.toFloat()),
+                BarEntry(5f, it.fridayCompleted.toFloat()),
+                BarEntry(6f, it.saturdayCompleted.toFloat()),
+                BarEntry(7f, it.sundayCompleted.toFloat())
+            )
+
+            initChart(binding.pomodoroChart, runningData, completedData)
+        })
 
         binding.alarmSoundButton.setOnClickListener {
             //startActivity
@@ -36,12 +68,15 @@ class UserProfileActivity : AppCompatActivity() {
             //startActivity
         }
 
+        binding.returnButton.setOnClickListener {
+            onBackPressed()
+        }
     }
 
-    private fun initChart(chart: CombinedChart) {
+    private fun initChart(chart: CombinedChart, runningData: List<BarEntry>, completedData: List<Entry>) {
         val data = CombinedData()
-        data.setData(initBarData())
-        data.setData(initLineData())
+        data.setData(initBarData(runningData))
+        data.setData(initLineData(completedData))
 
         chart.apply {
             description.isEnabled = false
@@ -94,9 +129,7 @@ class UserProfileActivity : AppCompatActivity() {
         chart.invalidate()
     }
 
-    private fun initBarData(): BarData {
-        val entries: ArrayList<BarEntry> = ArrayList()
-
+    private fun initBarData(entries: List<BarEntry>): BarData {
         val dataSet = BarDataSet(entries, getString(R.string.running_sessions))
         dataSet.apply {
             color = ContextCompat.getColor(this@UserProfileActivity, R.color.light_blue)
@@ -106,9 +139,7 @@ class UserProfileActivity : AppCompatActivity() {
         return BarData(dataSet)
     }
 
-    private fun initLineData(): LineData {
-        val entries: ArrayList<Entry> = ArrayList()
-
+    private fun initLineData(entries: List<Entry>): LineData {
         val dataSet = LineDataSet(entries, getString(R.string.completed_sessions))
         dataSet.apply {
             color = ContextCompat.getColor(this@UserProfileActivity, R.color.green)
@@ -127,5 +158,13 @@ class UserProfileActivity : AppCompatActivity() {
     private fun setStatsHelloText(userName: String) {
         binding.statsHelloText.text =
             "${getString(R.string.hey)} $userName! ${getString(R.string.your_stats_hello_text)}"
+    }
+
+    private fun setCompletedSessions(completedSessions: Int?) {
+        binding.completedSessions.text = completedSessions?.toString() ?: "0"
+    }
+
+    private fun setRunningSessions(runningSessions: Int?) {
+        binding.runningSessions.text = runningSessions?.toString() ?: "0"
     }
 }
